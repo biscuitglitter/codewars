6c6c3e3bcaa6bfe00a83988aa6d5026b0b75a533
miuhuru <zermoun.yasmine@gmail.com>
2022-03-12 22:35:26 +0100

    day three

--- katas.js
+++ katas.js
@@ -1,70 +1,71 @@
 function even_or_odd(number) {
-    if (number % 2 === 0) {
-      return "Even"
-    } 
-      if (number % 2 !== 0) {
-      return "Odd"
-    }
+  if (number % 2 === 0) {
+    return "Even"
+  }
+  if (number % 2 !== 0) {
+    return "Odd"
+  }
 }
-  
+
 function positiveSum(arr) {
-    return arr.filter(num => num > 0).reduce((prev, curr) => prev + curr, 0)}
-   
+  return arr.filter(num => num > 0).reduce((prev, curr) => prev + curr, 0)
+}
+
 const opposite = (number) => (number < 0) ? -number : -number
 
 const makeNegative = (num) => (num < 0) ? num : -num
 
 const filter_list = (l) => l.filter(item => Number.isInteger(item))
 
-const sumTwoSmallestNumbers = (numbers) => numbers.sort((prev, curr) => prev - curr).slice(0,2).reduce((prev, curr) => prev + curr, 0)
+const sumTwoSmallestNumbers = (numbers) => numbers.sort((prev, curr) => prev - curr).slice(0, 2).reduce((prev, curr) => prev + curr, 0)
 
 const repeatStr = (n, s) => (n < 0) ? s.repeat(-n) : s.repeat(n)
 
 const twoSum = (numbers, target) => {
   for (let i = 0; i < numbers.length; i++) {
-      for (let k = 0; k < numbers.length; k++) {
-              if (numbers[i] + numbers[k] === target && k !== i) {
-                return [i, k]
-              }
+    for (let k = 0; k < numbers.length; k++) {
+      if (numbers[i] + numbers[k] === target && k !== i) {
+        return [i, k]
+      }
     }
   }
 }
 
-function getCount (str) {
+function getCount(str) {
   let vowelsCount = 0;
   let array = Array.from(str)
   array.forEach(letter => {
-      if (letter.includes("a")) {
-          vowelsCount = vowelsCount + 1
-      }
-      if (letter.includes("e")) {
-          vowelsCount = vowelsCount + 1
-      }
-      if (letter.includes("i")) {
-          vowelsCount = vowelsCount + 1
-      }
-      if (letter.includes("o")) {
-          vowelsCount = vowelsCount + 1
-      }
-      if (letter.includes("u")) {
-          vowelsCount = vowelsCount + 1
-      }
+    if (letter.includes("a")) {
+      vowelsCount = vowelsCount + 1
+    }
+    if (letter.includes("e")) {
+      vowelsCount = vowelsCount + 1
+    }
+    if (letter.includes("i")) {
+      vowelsCount = vowelsCount + 1
+    }
+    if (letter.includes("o")) {
+      vowelsCount = vowelsCount + 1
+    }
+    if (letter.includes("u")) {
+      vowelsCount = vowelsCount + 1
+    }
   })
-    return vowelsCount;
+  return vowelsCount;
 }
-  
+
 const getCount = (str) => {
   let vowelsCount = 0
   for (i in str) {
-  switch (str[i]) {
-  case "a": 
-  case "e":
-  case "o":
-  case "i": 
-  case "u":
-  vowelsCount++
-  break;
-  }
+    switch (str[i]) {
+      case "a":
+      case "e":
+      case "o":
+      case "i":
+      case "u":
+        vowelsCount++
+        break;
+    }
   }
   return vowelsCount
 }
@@ -72,7 +73,28 @@ const getCount = (str) => {
 const getCount = (str) => {
   let vowelsCount = 0
   let res = str.match(/[aeiou]/g)
-  if (res === null) return vowelsCount 
+  if (res === null) return vowelsCount
   res.forEach(x => vowelsCount++)
-      return vowelsCount
+  return vowelsCount
+}
+
+const disemvowel = (str) => str.replace(/[aeiouAEIOU]/g, "")
+
+const getMiddle = (s) => {
+  let res = s.length % 2 === 0 ? "even" : "odd"
+  let num = res === "even" ? (s.length) / 2 : (s.length + 1) / 2
+  return res === "even" ? s.charAt(num - 1) + s.charAt(num) : s.charAt(num - 1)
+}
+
+const accum = (s) => {
+  let res = ""
+  let word = ""
+  Array.from(s).forEach(function (letter, index) {
+    res += s.charAt(index).repeat(index + 1)
+    console.log(res)
+  })
+  for (let i = 0; i < Array.from(s).length; i++) {
+    word = $res.charAt(i).repeat(i + 1)
   }
+}
+accum("abcd")
